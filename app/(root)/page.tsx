cbf69a411251a32757399752a0aca1b051925359
Anixii <kenjebekoverbol53@gmail.com>
2024-04-07 23:13:45 +0600

    Init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>  
+    Hello Admin 
+    <Button variant={'secondary'}>Click me</Button>
+    </>
+  );
+}
